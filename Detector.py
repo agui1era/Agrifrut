380111c82c0a6a84a8b7d5b80a765f6f1f20af48
Tu Nombre <you@example.com>
2020-11-18 12:00:08 -0300

    1

--- Detector.py
+++ Detector.py
@@ -9,48 +9,72 @@ import shutil # to save it locally
 
 
 def main():
+
+
   model='model.tflite'
   labels='labels.txt'
   img_input='input.jpg'
   img_output='output.jpg'
 
-    ## Set up the image URL and filename
-  image_url = "http://192.168.0.3:8080/?action=snapshot"
+  img_input2='input2.jpg'
+  img_output2='output2.jpg'
+
+  # Set up the image URL and filename
+  image_url = "http://192.168.0.6:8080/?action=snapshot"
+  image_url2 = "http://192.168.0.3:8080/?action=snapshot"
 
   filename =  img_input
+  filename2 =  img_input2
 
   # Initialize engine.
   engine = DetectionEngine(model)
   labels = dataset_utils.read_label_file(labels) 
 
   while cv2.waitKey(1) & 0xFF != ord('q'):
-
+    
+    print()
     print('____________________________________')
+    print()
+
     # Open the url image, set stream to True, this will return the stream content.
     r = requests.get(image_url, stream = True)
+    r2 = requests.get(image_url2, stream = True)
 
     # Check if the image was retrieved successfully
-    if r.status_code == 200:
+    if (r.status_code == 200) and (r2.status_code == 200) :
         # Set decode_content value to True, otherwise the downloaded image file's size will be zero.
         r.raw.decode_content = True
+        r2.raw.decode_content = True
         
         # Open a local file with wb ( write binary ) permission.
         with open(filename,'wb') as f:
             shutil.copyfileobj(r.raw, f)
             
-        print('Image sucessfully Downloaded: ',filename)
+        print('Image sucessfully Downloaded1 : ',filename)
+
+        # Open a local file with wb ( write binary ) permission.
+        with open(filename2,'wb') as f2:
+            shutil.copyfileobj(r2.raw, f2)
+            
+        print('Image sucessfully Downloaded2 : ',filename2)
+
+
     else:
-        print('Image Couldn\'t be retreived')
+        print('Images Couldn\'t be retreived')
     
     im = Image.open(filename)
     im = im.crop((150, 100, 500, 300))
     im.save(filename)
+
+    im2 = Image.open(filename2)
+    im2 = im2.crop((150, 100, 500, 300))
+    im2.save(filename2)
    
 
     # Open image.
     img = Image.open(img_input).convert('RGB')
     #Make the new image half the width and half the height of the original image
-    img = img.resize((round(img.size[0]*0.5), round(img.size[1]*0.5)))
+    img = img.resize((round(img.size[0])*2, round(img.size[1])*2))
  
     draw = ImageDraw.Draw(img)
    
@@ -62,6 +86,7 @@ def main():
                                     top_k=10)
 
     # Print and draw detected objects.
+    print('---------------- OBJETO 1 ----------------')
     for obj in objs:
       if labels:
         print(labels[obj.label_id])
@@ -74,8 +99,40 @@ def main():
       print('No objects detected.')
 
 
+   
+    # Open image.
+    img2 = Image.open(img_input2).convert('RGB')
+    #Make the new image half the width and half the height of the original image
+    img2 = img2.resize((round(img2.size[0])*2, round(img2.size[1])*2))
+ 
+    draw2 = ImageDraw.Draw(img2)
+   
+    # Run inference.
+    objs2 = engine.detect_with_image(img,
+                                    threshold=0.5,
+                                    keep_aspect_ratio='store_true',
+                                    relative_coord=False,
+                                    top_k=10)
+
+    # Print and draw detected objects.
+    print('---------------- OBJETO 2 ----------------')
+    for obj2 in objs2:
+      if labels:
+        print(labels[obj2.label_id])
+      print('score =', obj2.score)
+      box2 = obj2.bounding_box.flatten().tolist()
+      #print('box =', box)
+      draw.rectangle(box2, outline='yellow')
+
+    if not objs2:
+      print('No objects detected.')   
+
+
 
     #clasificando racimos
+
+
+  
 
 
 
