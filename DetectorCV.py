faa9e08777fbefb2bb6f317a4bacef89fd5c049b
Oscar <oscar@aguilera.xyz>
2021-03-08 19:02:15 -0300

    Mostrar ventana

--- DetectorCV.py
+++ DetectorCV.py
@@ -76,7 +76,7 @@ def main():
       image = cv2.imread(img_output) 
       cv2.namedWindow("window", cv2.WND_PROP_FULLSCREEN)
       #cv2.setWindowProperty("window",cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
-      #cv2.imshow("window",image)
+      cv2.imshow("window",image)
 
       #closing all open windows  
   cv2.destroyAllWindows()   
