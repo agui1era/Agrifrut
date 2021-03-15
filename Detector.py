6bed093dc25f9ba68b45ca9c456868ba519038f9
agui1era <oscar@aguilera.xyz>
2021-03-15 19:59:13 -0300

    fix

--- Detector.py
+++ Detector.py
@@ -80,66 +80,11 @@ def main():
     # Save image with bounding boxes.
     if img_output:
       img.save(img_output)
-
-      image = Image.open(img_output)
-      new_image = image.resize((960, 720))
-      new_image.save(img_output)
-
-      #concatenando imagnes
-      im1 = cv2.imread(img_output) 
-      im2 = cv2.imread('ffffff.png')
-      im3 = cv2.imread('ffffff.png')
-      
-
-      font = cv2.FONT_HERSHEY_SIMPLEX 
-  
-      # org 
-      org = (100, 100) 
-        
-      # fontScale 
-      fontScale = 2
-        
-      # Blue color in BGR 
-      color = (56, 56, 56) 
-      color2 = (128, 0, 255)
-        
-      # Line thickness of 2 px 
-      thickness = 2
-        
-      # Using cv2.putText() method 
-      cv2.putText(im2, 'NEGRA', org, font,  
-                        fontScale, color, thickness, cv2.LINE_AA) 
-
-      org = (100, 200) 
-
-         # Using cv2.putText() method 
-      cv2.putText(im2, str(final_negra)+"%", org, font,  
-                        fontScale, color, thickness, cv2.LINE_AA) 
-      org = (100, 100) 
-        
-      cv2.putText(im3, 'ROSADA', org, font,  
-                        fontScale, color2, thickness, cv2.LINE_AA) 
-      org = (100, 200) 
-
-      cv2.putText(im3, str(final_rosada)+"%", org, font,  
-                        fontScale, color2, thickness, cv2.LINE_AA) 
-                 
-  
-      im_h = cv2.hconcat([im2, im1])
-
-      im_h = cv2.hconcat([im_h, im3])
-      cv2.imwrite(img_output, im_h)
-
-      image = cv2.imread(img_output) 
-
-     
-      cv2.namedWindow("window", cv2.WND_PROP_FULLSCREEN)
-      #cv2.setWindowProperty("window",cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
-      #cv2.imshow("window",image)
-
-      
-
-          
+    image = cv2.imread(img_output) 
+    cv2.namedWindow("window", cv2.WND_PROP_FULLSCREEN)
+    #cv2.setWindowProperty("window",cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
+    #cv2.imshow("window",image)
+    
       #closing all open windows  
   cv2.destroyAllWindows()   
   cap.release()   
